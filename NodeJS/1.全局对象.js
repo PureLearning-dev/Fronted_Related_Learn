ee59c066fde869175cd79ea962dd82805ac30b31
PureLearning-dev <liu.jie.coder@gmail.com>
2026-06-08 15:12:57 +0800

    code: NodeJS 全局对象

---
+++ NodeJS/1.全局对象.js
@@ -0,0 +1,8 @@
+// 对于 setTimeout 和 setInterval 在 nodejs 环境仍然可以使用，甚至还多了一份关于 timer 的全局函数。
+
+// __dirname 可以访问当前路径。
+console.log(__dirname);
+// __filename 可以打印出当前路径 + 文件名。
+console.log(__filename);
+
+// 还有更多的全局对象，在需要的时候访问官网查询 api 则可。
